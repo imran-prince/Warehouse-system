b7908125be05f8d4d7fb5b09f9e9d60cf96498a6
imran-hossain <csebubt35@gmail.com>
2022-05-06 08:19:36 +0600

    login form add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
- 
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
+import Register from './Components/Register/Register';
 function App() {
   return (
     <div  >
       <Header></Header>
-       hellow mama
-      
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/logIn' element={<Login></Login>} ></Route>
+        <Route path='register' element={<Register></Register>}></Route>
+      </Routes>
+
+
     </div>
   );
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    const formSubmit=(event)=>{
+        event.preventDefault()
+    }
+    return (
+        <Form onSubmit={formSubmit} className='w-50 m-auto my-3'>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" name='email' />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" name='password' />
+            </Form.Group>
+          
+            <Button variant="primary" type="submit">
+                Log-In
+            </Button>
+            <Link to='/register' className='mx-5'>New Register</Link>
+        </Form>
+    );
+};
+
+export default Login;
